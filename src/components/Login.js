7e93c736750f2fddbf03f91263d7362bc9bd5a0e
Shobhit Agrawal <Shobhit.Agrawal@fiery.com>
2025-08-27 00:41:01 +0530

    project setup for netflix

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <Header />
+      <div>Login</div>
+    </div>
+  )
+}
+
+export default Login
