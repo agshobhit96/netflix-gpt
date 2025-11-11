6e51a3ac770c64d81d10c49f18128a3cda213ff1
Shobhit Agrawal <Shobhit.Agrawal@fiery.com>
2025-11-11 23:20:22 +0530

    sign in/ sign up

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from './Login';
 import Browse from './Browse';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Login />,
-    },
-    {
-        path: '/browse',
-        element: <Browse />,
-    },
-]);
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth}  from '../utils/firebase';
+import { useDispatch } from 'react-redux';
+import {addUser, removeUser} from '../utils/userSlice';
 
 const Body = () => {
+    const dispatch = useDispatch();
+
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <Login />,
+        },
+        {
+            path: '/browse',
+            element: <Browse />,
+        },
+    ]);
+
+    useEffect(() => {
+        // Kind of an event listener which listens to the auth changes
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/auth.user
+                const {uid, email, displayName} = user;
+                dispatch(addUser({ uid, email, displayName }));
+            } else {
+                // User is signed out
+                dispatch(removeUser());
+            }
+            });
+    }, []);
     return (
         <RouterProvider router={router} />
     );
